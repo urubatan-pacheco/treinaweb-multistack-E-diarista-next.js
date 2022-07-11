d1d6589eb6dcbb66b28cb1e4a6eddc6dd72694da
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-07-11 14:30:04 -0300

    01:08 - Configurando o Storybook

---
+++ .storybook/ediarista-theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming'
+
+
+export default create({
+    brandTitle: 'E-diarista',
+    brandImage: './img/logos/logo.svg',
+    brandUrl: 'https://treinaweb.com.br',
+})
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import '../src/UI/styles/globals.css'
+import StoryThemeProvider from '../src/UI/themes/StoryThemeProvider'
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +9,5 @@ export const parameters = {
     },
   },
 }
+
+export const decorators = [StoryThemeProvider];
