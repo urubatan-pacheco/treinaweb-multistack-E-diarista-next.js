89eccd9a4535a06b91a85e36cf4d145f1eab6ee6
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-07-12 16:27:37 -0300

    01:12 - Criando header mobile

--- src/UI/components/surfaces/Header/Header.tsx
+++ src/UI/components/surfaces/Header/Header.tsx
@@ -1,9 +1,22 @@
-import { Container, Toolbar } from "@mui/material";
+import {
+  Container,
+  Divider,
+  IconButton,
+  MenuItem,
+  MenuList,
+  Toolbar,
+} from "@mui/material";
 import Link from "UI/components/navigation/Link/Link";
-import { HeaderAppaBar, HeaderLogo, ButtonsContainer } from "./Header.styled";
+import {
+  HeaderAppaBar,
+  HeaderLogo,
+  ButtonsContainer,
+  HeaderDrawer,
+} from "./Header.styled";
 import RoundedButton from "UI/components/inputs/RoundedButton/RoundedButton";
+
 const Header: React.FC = () => {
-  return <HeaderDesktop />;
+  return <HeaderMobile />;
 };
 
 export default Header;
@@ -28,6 +41,32 @@ const HeaderDesktop: React.FC = () => {
           </Link>
           <Link href="/login">Login</Link>
         </ButtonsContainer>
+      </Toolbar>
+    </HeaderAppaBar>
+  );
+};
+
+const HeaderMobile: React.FC = () => {
+  return (
+    <HeaderAppaBar>
+      <Toolbar component={Container}>
+        <IconButton edge={"start"} color={"inherit"}>
+          <i className="twf-bars" />
+        </IconButton>
+        <Link href="/">
+          <HeaderLogo src="/img/logos/logo.svg" alt="e-diarista" />
+        </Link>
+        <HeaderDrawer open={false}>
+          <MenuList>
+            <Link href="/login" Component={MenuItem}>
+              Login
+            </Link>
+            <Divider />
+            <Link href="/cadastro/diarista" Component={MenuItem}>
+              Seja um(a) diarista
+            </Link>
+          </MenuList>
+        </HeaderDrawer>
       </Toolbar>
     </HeaderAppaBar>
   );
