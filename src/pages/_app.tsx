a6738e5fd312a8b8664a320af1ad6ab28d18bfe1
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-07-13 11:00:31 -0300

    01:16 - Arrumando o Layout da Página

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,25 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "@mui/material";
 import theme from "UI/themes/theme";
 import React from "react";
+import Header from "UI/components/surfaces/Header/Header";
+import Footer from "UI/components/surfaces/Footer/Footer";
+import { AppContainer } from "@styles/pages/AppContainer.styled";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>e-diarista {pageProps.title && ` - ${pageProps.title}`}</title>
+      </Head>
       <ThemeProvider theme={theme}>
-        <Component {...pageProps} />;
+        <AppContainer>
+          <Header />
+          <main>
+            <Component {...pageProps} />;
+          </main>
+          <Footer />
+        </AppContainer>
       </ThemeProvider>
     </>
   );
