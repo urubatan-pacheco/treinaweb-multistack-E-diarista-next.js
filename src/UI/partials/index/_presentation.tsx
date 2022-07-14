fc4a40250ceb5c4f58af052b7c95ad757c10b82c
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-07-14 12:32:14 -0300

    01:18 - Criando tela de apresentacao parte 2

--- src/UI/partials/index/_presentation.tsx
+++ src/UI/partials/index/_presentation.tsx
@@ -1,7 +1,16 @@
-import { SectionContainer } from "./_presentation.styled";
+import { ContaienrStyled, SectionContainer } from "./_presentation.styled";
 
 const Presentation = () => {
-  return <SectionContainer></SectionContainer>;
+  return (
+    <SectionContainer>
+      <ContaienrStyled>
+        <div>titulo adaw da daw da wd ad wa dwa da wda da dwa aw d</div>
+        <div>subtitulo</div>
+        <div>button</div>
+        <div>imagem</div>
+      </ContaienrStyled>
+    </SectionContainer>
+  );
 };
 
 export default Presentation;
