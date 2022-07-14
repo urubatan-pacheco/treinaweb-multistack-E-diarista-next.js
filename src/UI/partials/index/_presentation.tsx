b15bc3e565fb5507e967700c2625943a7fa0a760
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-07-14 10:48:26 -0300

    01:17 - Criando tela de apresentacao parte 1

---
+++ src/UI/partials/index/_presentation.tsx
@@ -0,0 +1,20 @@
+import {
+  ContainerStyled,
+  SectionContainer,
+  SectionTitle,
+} from "./_presentation.styled";
+
+const Presentation = () => {
+  return (
+    <SectionContainer>
+      <ContainerStyled>
+        <SectionTitle>
+          Encontre agora mesmo um(a) <em>diarista</em>
+          <i className="twf-search" />
+        </SectionTitle>
+      </ContainerStyled>
+    </SectionContainer>
+  );
+};
+
+export default Presentation;
