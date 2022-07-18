bcafce7d94ca19aba19d45b9098a874903192671
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-07-18 16:20:47 -0300

    1:24 - Criando tela de vantagens parte 3

--- src/UI/partials/index/_advantages.tsx
+++ src/UI/partials/index/_advantages.tsx
@@ -1,10 +1,89 @@
-import { Container } from "@mui/material";
-import { GradientBackground, SectionTitle } from "./_advantages.style";
+import { Container, ListItem, ListItemAvatar } from "@mui/material";
+import {
+  GradientBackground,
+  SectionTitle,
+  ListStyle,
+  AvatarStyled,
+  ListItemTextStyled,
+} from "./_advantages.style";
 const Advantages = () => {
   return (
     <GradientBackground>
       <Container>
         <SectionTitle>Por que usar o E-diaristas?</SectionTitle>
+        <ListStyle>
+          <ListItem>
+            <ListItemAvatar>
+              <AvatarStyled>
+                <i className="twf-certificate" />
+              </AvatarStyled>
+            </ListItemAvatar>
+            <ListItemTextStyled
+              primary={"Diversidade"}
+              secondary={"São mais de 5.000 profissionais esperando por você!"}
+            />
+          </ListItem>
+
+          <ListItem>
+            <ListItemAvatar>
+              <AvatarStyled>
+                <i className="twf-certificate" />
+              </AvatarStyled>
+            </ListItemAvatar>
+            <ListItemTextStyled
+              primary={"Diversidade"}
+              secondary={"São mais de 5.000 profissionais esperando por você!"}
+            />
+          </ListItem>
+
+          <ListItem>
+            <ListItemAvatar>
+              <AvatarStyled>
+                <i className="twf-certificate" />
+              </AvatarStyled>
+            </ListItemAvatar>
+            <ListItemTextStyled
+              primary={"Diversidade"}
+              secondary={"São mais de 5.000 profissionais esperando por você!"}
+            />
+          </ListItem>
+
+          <ListItem>
+            <ListItemAvatar>
+              <AvatarStyled>
+                <i className="twf-certificate" />
+              </AvatarStyled>
+            </ListItemAvatar>
+            <ListItemTextStyled
+              primary={"Diversidade"}
+              secondary={"São mais de 5.000 profissionais esperando por você!"}
+            />
+          </ListItem>
+
+          <ListItem>
+            <ListItemAvatar>
+              <AvatarStyled>
+                <i className="twf-certificate" />
+              </AvatarStyled>
+            </ListItemAvatar>
+            <ListItemTextStyled
+              primary={"Diversidade"}
+              secondary={"São mais de 5.000 profissionais esperando por você!"}
+            />
+          </ListItem>
+
+          <ListItem>
+            <ListItemAvatar>
+              <AvatarStyled>
+                <i className="twf-certificate" />
+              </AvatarStyled>
+            </ListItemAvatar>
+            <ListItemTextStyled
+              primary={"Diversidade"}
+              secondary={"São mais de 5.000 profissionais esperando por você!"}
+            />
+          </ListItem>
+        </ListStyle>
       </Container>
     </GradientBackground>
   );
