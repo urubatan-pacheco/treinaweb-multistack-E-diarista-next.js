ea95fcbd61cf01eb107b76a08d067330c93bde24
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-07-19 00:19:37 -0300

    1:25 - Criando tela de vantagens parte 4

--- src/UI/partials/index/_advantages.tsx
+++ src/UI/partials/index/_advantages.tsx
@@ -1,88 +1,71 @@
 import { Container, ListItem, ListItemAvatar } from "@mui/material";
+import React from "react";
 import {
   GradientBackground,
   SectionTitle,
   ListStyle,
   AvatarStyled,
   ListItemTextStyled,
+  ListDivider,
 } from "./_advantages.style";
 const Advantages = () => {
+  const advantagesList = [
+    {
+      icon: "twf-woman",
+      title: "Diversidade",
+      description: "São mais de 5.000 profissionais esperando por você!",
+    },
+    {
+      icon: "twf-certificate",
+      title: "Confiabilidade",
+      description: "Todos os profissionais são verificados",
+    },
+    {
+      icon: "twf-search-2",
+      title: "Rastreabilidade",
+      description: "Você pode acessar todo o histórico do(a) profissional",
+    },
+    {
+      icon: "twf-frame-broken",
+      title: "Segurança",
+      description: "Seguro sobre qualquer possível dano",
+    },
+    {
+      icon: "twf-payment",
+      title: "Controle",
+      description:
+        "O pagamento é realizado somente quando o(a) profissional está na sua casa",
+    },
+    {
+      icon: "twf-broom-bucket",
+      title: "Experiência",
+      description: "Mais de 50.000 diárias realizadas",
+    },
+  ];
+
   return (
     <GradientBackground>
       <Container>
         <SectionTitle>Por que usar o E-diaristas?</SectionTitle>
         <ListStyle>
-          <ListItem>
-            <ListItemAvatar>
-              <AvatarStyled>
-                <i className="twf-certificate" />
-              </AvatarStyled>
-            </ListItemAvatar>
-            <ListItemTextStyled
-              primary={"Diversidade"}
-              secondary={"São mais de 5.000 profissionais esperando por você!"}
-            />
-          </ListItem>
-
-          <ListItem>
-            <ListItemAvatar>
-              <AvatarStyled>
-                <i className="twf-certificate" />
-              </AvatarStyled>
-            </ListItemAvatar>
-            <ListItemTextStyled
-              primary={"Diversidade"}
-              secondary={"São mais de 5.000 profissionais esperando por você!"}
-            />
-          </ListItem>
-
-          <ListItem>
-            <ListItemAvatar>
-              <AvatarStyled>
-                <i className="twf-certificate" />
-              </AvatarStyled>
-            </ListItemAvatar>
-            <ListItemTextStyled
-              primary={"Diversidade"}
-              secondary={"São mais de 5.000 profissionais esperando por você!"}
-            />
-          </ListItem>
-
-          <ListItem>
-            <ListItemAvatar>
-              <AvatarStyled>
-                <i className="twf-certificate" />
-              </AvatarStyled>
-            </ListItemAvatar>
-            <ListItemTextStyled
-              primary={"Diversidade"}
-              secondary={"São mais de 5.000 profissionais esperando por você!"}
-            />
-          </ListItem>
-
-          <ListItem>
-            <ListItemAvatar>
-              <AvatarStyled>
-                <i className="twf-certificate" />
-              </AvatarStyled>
-            </ListItemAvatar>
-            <ListItemTextStyled
-              primary={"Diversidade"}
-              secondary={"São mais de 5.000 profissionais esperando por você!"}
-            />
-          </ListItem>
-
-          <ListItem>
-            <ListItemAvatar>
-              <AvatarStyled>
-                <i className="twf-certificate" />
-              </AvatarStyled>
-            </ListItemAvatar>
-            <ListItemTextStyled
-              primary={"Diversidade"}
-              secondary={"São mais de 5.000 profissionais esperando por você!"}
-            />
-          </ListItem>
+          {advantagesList.map((item, index) => {
+            return (
+              <React.Fragment key={item.icon}>
+                {index !== 0 && <ListDivider />}
+                <ListItem>
+                  <ListItemAvatar>
+                    <AvatarStyled>
+                      <i className={item.icon} />
+                    </AvatarStyled>
+                  </ListItemAvatar>
+                  <ListItemTextStyled
+                    primary={item.title}
+                    secondary={item.description}
+                  />
+                </ListItem>
+              </React.Fragment>
+            );
+          })}
         </ListStyle>
       </Container>
     </GradientBackground>
