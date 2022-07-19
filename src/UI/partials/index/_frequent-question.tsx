3e92354622ad68d4860cdd7ca90594b68259c2d7
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-07-19 13:18:54 -0300

    1:27 - Criando tela de perguntas frequentes parte 2

--- src/UI/partials/index/_frequent-question.tsx
+++ src/UI/partials/index/_frequent-question.tsx
@@ -1,16 +1,34 @@
 import {
+  AccordionDetails,
+  AccordionSummary,
+  Container,
+  Typography,
+} from "@mui/material";
+import {
   Wave,
   SectionContainer,
   SectionTitle,
   SectionSubTitle,
+  AccondionStyled,
 } from "./_frequent-question.styled";
 
 const FrequestQuestion = () => {
   return (
     <SectionContainer>
       <Wave src={"/img/home/waves.svg"} />
-      <SectionTitle>Ainda está com dúvidas ?</SectionTitle>
-      <SectionSubTitle>Veja abaixo as perguntas frequentes</SectionSubTitle>
+      <Container>
+        <SectionTitle>Ainda está com dúvidas ?</SectionTitle>
+        <SectionSubTitle>Veja abaixo as perguntas frequentes</SectionSubTitle>
+        <AccondionStyled>
+          <AccordionSummary expandIcon={<i className="twf-minus" />}>
+            <Typography color={"primary"}>dwada</Typography>
+          </AccordionSummary>
+
+          <AccordionDetails>
+            <Typography>dwadawdad</Typography>
+          </AccordionDetails>
+        </AccondionStyled>
+      </Container>
     </SectionContainer>
   );
 };
