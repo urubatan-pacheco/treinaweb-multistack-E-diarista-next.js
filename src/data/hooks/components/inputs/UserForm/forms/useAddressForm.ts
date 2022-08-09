ffabfea21fafb634006d8b6a0da612a408bd0362
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-08-09 15:47:34 -0300

    l

--- src/data/hooks/components/inputs/UserForm/forms/useAddressForm.ts
+++ src/data/hooks/components/inputs/UserForm/forms/useAddressForm.ts
@@ -1,11 +1,26 @@
 import { FormValues } from "data/@types/forms/FormValue";
+import useCities from "data/hooks/useCities";
+import { LocationService } from "data/services/LocationService";
+import { useMemo } from "react";
 import { useFormContext } from "react-hook-form";
+import ItemCounter from "UI/components/inputs/ItemCounter/ItemCounter";
 
 export default function useAddressForm() {
   const {
-    register,
-    control,
-    watch,
-    formState: { errors },
-  } = useFormContext<FormValues>();
+      register,
+      control,
+      watch,
+      formState: { errors },
+    } = useFormContext<FormValues>(),
+    [addressState, addressCity, addressCep] = watch([
+      "endereco.estado",
+      "endereco.cidade",
+      "endereco.cep",
+    ]),
+    estados = LocationService.estados(),
+    listaCidades = useCities(addressState),
+    opcoesCidades = useMemo(
+      () => listaCidades.map((item) => item.cidade),
+      [listaCidades]
+    );
 }
