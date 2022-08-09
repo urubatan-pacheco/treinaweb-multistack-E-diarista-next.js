1da02e0ba1e5e030fa0482b799ddbfc157b34d5c
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-08-09 17:27:04 -0300

    03:16 - montando componente de endereço

--- src/data/hooks/components/inputs/UserForm/forms/useAddressForm.ts
+++ src/data/hooks/components/inputs/UserForm/forms/useAddressForm.ts
@@ -1,7 +1,7 @@
 import { FormValues } from "data/@types/forms/FormValue";
 import useCities from "data/hooks/useCities";
 import { LocationService } from "data/services/LocationService";
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 import { useFormContext } from "react-hook-form";
 import ItemCounter from "UI/components/inputs/ItemCounter/ItemCounter";
 
@@ -10,6 +10,7 @@ export default function useAddressForm() {
       register,
       control,
       watch,
+      setValue,
       formState: { errors },
     } = useFormContext<FormValues>(),
     [addressState, addressCity, addressCep] = watch([
@@ -20,8 +21,51 @@ export default function useAddressForm() {
     estados = LocationService.estados(),
     listaCidades = useCities(addressState),
     opcoesCidades = useMemo(
-      
       () => listaCidades.map((item) => item.cidade),
       [listaCidades]
     );
+
+  useEffect(() => {
+    register("endereco.codigo_ibge");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    if (addressCity) {
+      const cidade = listaCidades.find((item) => item.cidade === addressCity);
+      if (cidade) {
+        setValue("endereco.codigo_ibge", cidade.codigo_ibge);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [addressCity]);
+
+  useEffect(() => {
+    const cep = (addressCep ?? "").replaceAll("_", "");
+    if (cep.length === 10) {
+      LocationService.cep(cep).then((newAddress) => {
+        if (newAddress) {
+          newAddress.uf && setValue("endereco.estado", newAddress.uf);
+          newAddress.localidade &&
+            setValue("endereco.cidade", newAddress.localidade);
+          newAddress.ibge && setValue("endereco.codigo_ibge", newAddress.ibge);
+          newAddress.bairro && setValue("endereco.bairro", newAddress.bairro);
+          newAddress.logradouro &&
+            setValue("endereco.logradouro", newAddress.logradouro);
+        }
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [addressCep]);
+
+  return {
+    addressCep,
+    addressCity,
+    addressState,
+    register,
+    estados,
+    opcoesCidades,
+    control,
+    errors,
+  };
 }
