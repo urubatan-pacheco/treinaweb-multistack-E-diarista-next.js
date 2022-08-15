1b20c963bf1111f2f66775015966247ebfdd1802
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-08-15 14:04:32 -0300

    03:06 - Observacoes

--- src/UI/partials/encontrar-diarista/_detalhe-servico.tsx
+++ src/UI/partials/encontrar-diarista/_detalhe-servico.tsx
@@ -1,4 +1,4 @@
-import { Divider, Tooltip, Typography } from "@mui/material";
+import { Button, Container, Divider, Tooltip, Typography } from "@mui/material";
 import { ServicoInterface } from "data/@types/ServicoInterface";
 import React, { PropsWithChildren } from "react";
 import { useFormContext, Controller } from "react-hook-form";
@@ -10,9 +10,11 @@ import { AddressForm } from "UI/components/inputs/UserForm/UserForm";
 import TextFieldMask from "UI/components/inputs/TextFieldMask/TextFieldMask";
 import { ItemsContainer } from "./_detalhe-servico.styled";
 import { FormValues } from "data/@types/forms/FormValue";
-
+import TextField from "UI/components/inputs/TextField/TextField";
 interface DetalheServicoProps {
   servicos?: ServicoInterface[];
+  comodos?: number;
+  podemosAtender?: boolean;
 }
 
 const houseParts = [
@@ -36,7 +38,11 @@ const houseParts = [
   { singular: "Outros", plural: "Outros", name: "quantidade_outros" },
 ];
 
-const DetalheServico: React.FC<DetalheServicoProps> = ({ servicos = [] }) => {
+const DetalheServico: React.FC<DetalheServicoProps> = ({
+  servicos = [],
+  comodos = 0,
+  podemosAtender,
+}) => {
   const {
     register,
     control,
@@ -149,15 +155,46 @@ const DetalheServico: React.FC<DetalheServicoProps> = ({ servicos = [] }) => {
                   label={"Hora Término"}
                   error={errors?.faxina?.hora_termino !== undefined}
                   helperText={errors?.faxina?.hora_termino?.message}
+                  fullWidth
                 />
               </div>
             </Tooltip>
           )}
         />
       </ItemsContainer>
+      <Divider sx={{ my: 5 }} />
+      <Typography sx={{ fontWeight: "bold", pb: 2 }}>Observações</Typography>
+
+      <TextField
+        label={"Quer acrescentar algum detalhe?"}
+        {...register("faxina.observacoes")}
+        required={false}
+        fullWidth
+        multiline
+      />
 
       <Divider sx={{ my: 5 }} />
+      <Typography sx={{ fontWeight: "bold", pb: 2 }}>
+        Qual endereço onde será realizada a limpeza?
+      </Typography>
       <AddressForm />
+
+      {!podemosAtender && (
+        <Typography color={"error"} align={"center"} sx={{ pb: 2 }}>
+          Infelizmente ainda não atendemos na sua região
+        </Typography>
+      )}
+
+      <Container sx={{ textAlign: "right" }}>
+        <Button
+          variant="contained"
+          color="secondary"
+          type="submit"
+          disabled={comodos === 0 || !podemosAtender}
+        >
+          Ir para identificação
+        </Button>
+      </Container>
     </div>
   );
 };
