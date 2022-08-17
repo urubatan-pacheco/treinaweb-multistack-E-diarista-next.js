a2747e3580f37ccdfe09be7c3c88c47d1df6db3a
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-08-17 16:42:43 -0300

    03:06 - instalando swr

--- src/data/hooks/pages/useContratacao.page.ts
+++ src/data/hooks/pages/useContratacao.page.ts
@@ -10,6 +10,7 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { FormSchemaService } from "data/services/FormSchemaService";
 import { ServicoInterface } from "data/@types/ServicoInterface";
+import useSwr from "swr";
 
 export default function useContratacao() {
   const [step, setStep] = useState(3),
